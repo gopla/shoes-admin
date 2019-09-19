176961bc9aa890b7a2059a2cec81b8ab0aac82df
unknown <aqibalvaaiziin@gmail.com>
2019-09-19 13:15:30 +0700

    lala

--- application/views/user/home/index.php
+++ application/views/user/home/index.php
@@ -1,46 +1,40 @@
-<section id="home-section" class="hero" style="background-color:white;"
-	data-stellar-background-ratio="0.5">
-	<div class="home-slider owl-carousel">
-		<div class="slider-item js-fullheight">
-			<div class="overlay"></div>
-			<div class="container-fluid p-0">
-				<div class="row d-md-flex no-gutters slider-text align-items-end justify-content-center"
-					data-scrollax-parent="true">
-					<img class="one-fourt align order-md-last img-fluid" src="https://images.pexels.com/photos/1449053/pexels-photo-1449053.jpeg?cs=srgb&dl=adult-aroma-aromatherapy-1449053.jpg&fm=jpg" alt="">
-					<div class="one-forth d-flex align-items-center ftco-animate"
-						data-scrollax=" properties: { translateY: '70%' }">
-						<div class="text mt-5">
-							<span class="subheading">Camille Salon</span>
-							<h1 class="mb-4">Get Pretty Look</h1>
-							<p class="mb-4">A small river named Duden flows by their place and supplies it with the
-								necessary regelialia. It is a paradisematic country.</p>
 
-							<p><a href="#" class="btn btn-primary py-3 px-4">Discover Now</a></p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+    <section id="home-section" class="hero" style="background-image: url(https://image.freepik.com/free-vector/blue-abstract-acrylic-brush-stroke-textured-background_53876-86373.jpg);" data-stellar-background-ratio="0.5">
+		  <div class="home-slider owl-carousel">
+	      <div class="slider-item js-fullheight">
+	      	<div class="overlay"></div>
+	        <div class="container-fluid p-0">
+	          <div class="row d-md-flex no-gutters slider-text align-items-end justify-content-end" data-scrollax-parent="true">
+	          	<img class="one-third align-self-end order-md-last img-fluid" src="images/bg_1.png" alt="">
+		          <div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+		          	<div class="text mt-5">
+		          		<span class="subheading">Camille Salon</span>
+			            <h1 class="mb-4">Get Pretty Look</h1>
+			            <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
+			            
+			            <p><a href="#" class="btn btn-secondary py-3 px-4">Discover Now</a></p>
+		            </div>
+		          </div>
+	        	</div>
+	        </div>
+	      </div>
 
-		<div class="slider-item js-fullheight">
-			<div class="overlay"></div>
-			<div class="container-fluid p-0">
-				<div class="row d-flex no-gutters slider-text align-items-center justify-content-end"
-					data-scrollax-parent="true">
-					<img class="one-third align-self-end order-md-last img-fluid" src="images/bg_2.png" alt="">
-					<div class="one-forth d-flex align-items-center ftco-animate"
-						data-scrollax=" properties: { translateY: '70%' }">
-						<div class="text mt-5">
-							<span class="subheading">Natural Beauty</span>
-							<h1 class="mb-4">Beauty Salon</h1>
-							<p class="mb-4">A small river named Duden flows by their place and supplies it with the
-								necessary regelialia. It is a paradisematic country.</p>
-
-							<p><a href="#" class="btn btn-primary py-3 px-4">Discover Now</a></p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+	      <div class="slider-item js-fullheight">
+	      	<div class="overlay"></div>
+	        <div class="container-fluid p-0">
+	          <div class="row d-flex no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
+	          	<img class="one-third align-self-end order-md-last img-fluid" src="images/bg_2.png" alt="">
+		          <div class="one-forth d-flex align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+		          	<div class="text mt-5">
+		          		<span class="subheading">Natural Beauty</span>
+			            <h1 class="mb-4">Beauty Salon</h1>
+			            <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country.</p>
+			            
+			            <p><a href="#" class="btn btn-primary py-3 px-4">Discover Now</a></p>
+		            </div>
+		          </div>
+	        	</div>
+	        </div>
+	      </div>
+	    </div>
+    </section>
