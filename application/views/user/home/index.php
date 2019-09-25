7c9eec3f56b6304655878bd001d7417ee5d9d59f
aqibalvaaiziin <aqibalvaaiziin@gmail.com>
2019-09-25 21:43:24 +0700

    service page

--- application/views/user/home/index.php
+++ application/views/user/home/index.php
@@ -6,30 +6,48 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="h-25 d-inline-block" style="width:100%" src="https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="First slide">
-		<div class="carousel-caption d-none d-md-block">
-		<h5>...</h5>
-		<p>...</p>
-  	</div>
+      <img class="d-inline-block" style="width:100%;height:982px" src="https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="First slide">
+		<div class="carousel-caption d-none d-md-block align-top text-left" style="transform:translate(-100px,-340px);z-index:5;">
+			<div style="width:85px;height:120px;border:5px solid #fff"></div>
+			<h1 class="font-weight-bold" style="font-size:3rem;color:#fff">Belle Crown Hotel</h1>
+			<h5 style="font-weight:500;color:#fff">This is The Beautifull Places to Get Relaxation </h5>
+			<a href="#about" class="btn btn-light font-weight-bold py-3 px-5">About Us</a>
+			<button class="btn btn-light font-weight-bold py-3 px-5 ml-5">Booking Now</button>
+  		</div>
 	</div>
     <div class="carousel-item">
-      <img class="h-25 d-inline-block" style="width:100%" src="https://images.pexels.com/photos/573552/pexels-photo-573552.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Second slide">
-		<div class="carousel-caption d-none d-md-block">
-		<h5>...</h5>
-		<p>...</p>
+      <img class="d-inline-block" style="width:100%;height:982px" src="https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Second slide">
+		<div class="carousel-caption d-none d-md-block align-top text-left" style="transform:translate(-100px,-280px);z-index:5;">
+			<div style="width:85px;height:120px;border:5px solid #3a3a40;"></div>
+			<h1 class="font-weight-bold" style="font-size:3rem;color:#3a3a40">Belle Crown Hotel</h1>
+			<h5 style="font-weight:500;color:#3a3a40">This is The Beautifull Places to Get Relaxation </h5>
+			<a href="#about" class="btn btn-dark font-weight-bold py-3 px-5">About Us</a>
+			<button class="btn btn-dark font-weight-bold py-3 px-5 ml-5">Booking Now</button>
+  		</div>
   	</div>
-	</div>
     <div class="carousel-item">
-      <img class="h-25 d-inline-block" style="width:100%" src="https://images.pexels.com/photos/2883048/pexels-photo-2883048.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Third slide">
-		<div class="carousel-caption d-none d-md-block">
-		<h5>...</h5>
-		<p>...</p>
+      <img class="d-inline-block" style="width:100%;height:982px" src="https://images.pexels.com/photos/262048/pexels-photo-262048.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Third slide">
+		<div class="carousel-caption d-none d-md-block align-top text-left" style="transform:translate(-100px,-340px);z-index:5;">
+			<div style="width:85px;height:120px;border:5px solid black"></div>
+			<h1 class="font-weight-bold" style="font-size:3rem;color:black">Belle Crown Hotel</h1>
+			<h5 style="font-weight:500;color:black">This is The Beautifull Places to Get Relaxation </h5>
+			<a href="#about" class="btn btn-light font-weight-bold py-3 px-5">About Us</a>
+			<button class="btn btn-light font-weight-bold py-3 px-5 ml-5">Booking Now</button>
+  		</div>
   	</div>
 	</div>
-  </div>
+	<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
 </div>
 
-<section class="ftco-section ftco-no-pb ftco-no-pt mt-3">
+<section class="ftco-section ftco-no-pb ftco-no-pt mt-5" id="about">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 d-flex">
@@ -56,153 +74,3 @@
 		</div>
 	</div>
 </section>
-
-<section class="ftco-section ftco-pricing mt-3">
-	<div class="container">
-		<div class="row justify-content-center pb-3">
-			<div class="col-md-10 heading-section text-center ftco-animate">
-				<h1 class="big">Pricing</h1>
-				<span class="subheading">Pricing</span>
-				<h2 class="mb-4">Our Prices</h2>
-				<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-3 ftco-animate">
-				<div class="pricing-entry pb-5 text-center">
-					<div>
-						<h3 class="mb-4">Hair Style</h3>
-						<p><span class="price">$50.00</span> <span class="per">/ session</span></p>
-					</div>
-					<ul>
-						<li>Hair Dryer</li>
-						<li>Hair Coloring</li>
-						<li>Hair Cut</li>
-						<li>Hair Dresser</li>
-						<li>Hair Spa</li>
-					</ul>
-					<p class="button text-center"><a href="#" class="btn btn-primary px-4 py-3">Get Offer</a></p>
-				</div>
-			</div>
-			<div class="col-md-3 ftco-animate">
-				<div class="pricing-entry pb-5 text-center">
-					<div>
-						<h3 class="mb-4">Manicure Pedicure</h3>
-						<p><span class="price">$34.50</span> <span class="per">/ session</span></p>
-					</div>
-					<ul>
-						<li>Manicure</li>
-						<li>Pedicure</li>
-						<li>Coloring</li>
-						<li>Nails</li>
-						<li>Nail Cut</li>
-					</ul>
-					<p class="button text-center"><a href="#" class="btn btn-primary px-4 py-3">Get Offer</a></p>
-				</div>
-			</div>
-			<div class="col-md-3 ftco-animate">
-				<div class="pricing-entry active pb-5 text-center">
-					<div>
-						<h3 class="mb-4">Makeup</h3>
-						<p><span class="price">$54.50</span> <span class="per">/ session</span></p>
-					</div>
-					<ul>
-						<li>Makeup</li>
-						<li>Professional Makeup</li>
-						<li>Blush On</li>
-						<li>Facial Massage</li>
-						<li>Facial Spa</li>
-					</ul>
-					<p class="button text-center"><a href="#" class="btn btn-primary px-4 py-3">Get Offer</a></p>
-				</div>
-			</div>
-			<div class="col-md-3 ftco-animate">
-				<div class="pricing-entry pb-5 text-center">
-					<div>
-						<h3 class="mb-4">Body Treatment</h3>
-						<p><span class="price">$89.50</span> <span class="per">/ session</span></p>
-					</div>
-					<ul>
-						<li>Massage</li>
-						<li>Spa</li>
-						<li>Foot Spa</li>
-						<li>Body Spa</li>
-						<li>Relaxing</li>
-					</ul>
-					<p class="button text-center"><a href="#" class="btn btn-primary px-4 py-3">Get Offer</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-<footer class="ftco-footer ftco-section">
-	<div class="container">
-		<div class="row mb-5">
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4">
-					<h2 class="ftco-heading-2 logo">Camille</h2>
-					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
-						live the blind texts.</p>
-					<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-						<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-						<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-						<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4 ml-md-5">
-					<h2 class="ftco-heading-2">Information</h2>
-					<ul class="list-unstyled">
-						<li><a href="#" class="py-2 d-block">FAQs</a></li>
-						<li><a href="#" class="py-2 d-block">Privacy</a></li>
-						<li><a href="#" class="py-2 d-block">Terms Condition</a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4">
-					<h2 class="ftco-heading-2">Links</h2>
-					<ul class="list-unstyled">
-						<li><a href="#" class="py-2 d-block">Home</a></li>
-						<li><a href="#" class="py-2 d-block">About</a></li>
-						<li><a href="#" class="py-2 d-block">Services</a></li>
-						<li><a href="#" class="py-2 d-block">Work</a></li>
-						<li><a href="#" class="py-2 d-block">Blog</a></li>
-						<li><a href="#" class="py-2 d-block">Contact</a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-md">
-				<div class="ftco-footer-widget mb-4">
-					<h2 class="ftco-heading-2">Have a Questions?</h2>
-					<div class="block-23 mb-3">
-						<ul>
-							<li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View,
-									San Francisco, California, USA</span></li>
-							<li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929
-										210</span></a></li>
-							<li><a href="#"><span class="icon icon-envelope"></span><span
-										class="text">info@yourdomain.com</span></a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-12 text-center">
-
-				<p>
-					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-					Copyright &copy;<script>
-						document.write(new Date().getFullYear());
-
-					</script> All rights reserved | This template is made with <i class="icon-heart color-danger"
-						aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-				</p>
-			</div>
-		</div>
-	</div>
-</footer>
